d1ff8ba8fa0a5a6de96631facfcf8586a8ae5c84
Jonas Valiunas <Jonas.Valiunas@bentley.com>
2018-01-27 01:10:16 +0200

    wip: rotating portal

---
+++ gamejamproject/Assets/RotateStepThroughPortal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateStepThroughPortal : StepThroughPortal
+{
+
+    protected override void _DoPortalAction(Collider player)
+    {
+        Vector3 xVec = new Vector3(1, 0, 0);
+        transform.parent.RotateAround(player.transform.position, xVec, 10);
+        //player.transform.position = targetPortal.transform.position + player.transform.forward * 1;
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
