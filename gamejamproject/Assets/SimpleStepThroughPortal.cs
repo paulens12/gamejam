003e0d0fbd9d7a04f3b2dfecb6e60c98432de317
Jonas Valiunas <Jonas.Valiunas@bentley.com>
2018-01-27 00:03:46 +0200

    repush things

---
+++ gamejamproject/Assets/SimpleStepThroughPortal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleStepThroughPortal : StepThroughPortal
+{
+
+
+    protected override void _DoPortalAction(Collider player)
+    {
+        player.transform.position = targetPortal.transform.position + player.transform.forward * 1;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
