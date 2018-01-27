f4c64057ddb6d7b437204ebc1b776af31caff6c2
Jonas Valiunas <Jonas.Valiunas@bentley.com>
2018-01-27 19:39:49 +0200

    add simpletransformationstrategy

---
+++ gamejamproject/Assets/SimpleTransformationStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTransformationStrategy : TransformationStrategy
+{
+
+    public override void DoTransform(Player player, StepThroughPortal targetPortal)
+    {
+        player.transform.position = targetPortal.transform.position + player.transform.forward * 1;
+    }
+}
