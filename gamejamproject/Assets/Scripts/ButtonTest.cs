dfa18cb46f09906c84fd48a3624bdb1329a51d8b
Paulius <pauliusval99@hotmail.com>
2018-01-27 03:10:48 +0200

    brainfart

---
+++ gamejamproject/Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour, IButtonScript {
+
+	public void Activate()
+    {
+        Debug.Log("ouch!");
+        transform.Rotate(30, 0, 0);
+        Invoke("RotateBack", 0.5f);
+    }
+
+    private void RotateBack()
+    {
+        transform.Rotate(-30, 0, 0);
+    }
+}
---
+++ gamejamproject/Assets/Scripts/CrosshairScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IButtonScript
+{
+    void Activate();
+}
+
+public class CrosshairScript : MonoBehaviour {
+
+    public float MaxDistance;
+
+    private bool debounce = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Fire1") <= 0)
+            debounce = false;
+        if (debounce)
+            return;
+        Ray ray = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, MaxDistance))
+        {
+            var script = hit.transform.GetComponent<IButtonScript>();
+            if (script != null)
+            {
+                Debug.Log("looking at it!");
+                if (Input.GetAxis("Fire1") > 0)
+                {
+                    debounce = true;
+                    script.Activate();
+                }
+            }
+        }
+	}
+}
