0367d02b4801b8d20ae21421abd503626d6b588d
Jonas Valiunas <Jonas.Valiunas@bentley.com>
2018-01-27 23:00:12 +0200

    improve the locked portal

--- gamejamproject/Assets/LockedSeeThroughPortal.cs
+++ gamejamproject/Assets/LockedSeeThroughPortal.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class LockedSeeThroughPortal : SeeThroughPortal {
 
-    public string key;
+    public string m_key;
+    public int m_numberRequired;
+    public bool m_consumeKeys;
 
     protected override void _DoPortalAction(Collider playerColider)
     {
         Player player = playerColider.transform.GetComponent<Player>();
-        if (player.requestObjectiveInstances("Button1Pressed"))
+
+
+
+        if (player.hasObjectiveDone(m_key))
         {
+            if (m_consumeKeys)
+                player.requestObjectiveInstances(m_key);
             base._DoPortalAction(playerColider);
         }
     }
