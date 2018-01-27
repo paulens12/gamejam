c38494a718311907cbbb0a45594bd4927f59e9ae
RookieMaster <arturas.genys@mif.stud.vu.lt>
2018-01-27 06:13:22 +0200

    Added distance meter UI
    Text based atm.

---
+++ gamejamproject/Assets/Scripts/DistanceCountUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceCountUI : MonoBehaviour
+{
+    public GameObject first;
+    public GameObject second;
+    public Text text;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(UpdateInformation());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    IEnumerator UpdateInformation()
+    {
+        while (true)
+        {
+            float dist = Vector3.Distance(first.transform.position, second.transform.position);
+            text.text = "Distance till object:" + dist;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
