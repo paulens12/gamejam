6611b5413182586393c76011587fa65326abd559
RookieMaster <agrookie@gmail.com>
2018-01-28 11:58:16 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ gamejamproject/Assets/GrabFunction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabFunction : MonoBehaviour {
+
+    public string type;
+    public GameObject triggerBox;
+    public GameObject model;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void Grabbed()
+    {
+
+    }
+
+
+
+
+}
---
+++ gamejamproject/Assets/Scripts/UI_ShowKeys.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ShowKeys : MonoBehaviour {
+
+
+    public GameObject[] Panels; 
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator UpdateGUI()
+    {
+        while (true)
+        {
+            foreach(GameObject obj in Panels)
+            {
+
+            }
+
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
