05d166ee3c46e1c51f12d6a978793eecb012c6cc
RookieMaster <agrookie@gmail.com>
2018-01-28 12:32:54 +0200

    test

---
+++ gamejamproject/Assets/InventoryUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InventoryUI : MonoBehaviour {
+
+    public Text text;
+    public GameObject Player;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator updateGUI()
+    {
+        while (true)
+        {
+            string textToOutput = "";
+            foreach(string input in )
+
+
+
+        }
+    }
+
+
+
+}
