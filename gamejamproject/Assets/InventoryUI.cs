65efb4c32e3889d22b58dc8bcc469dca6f70b755
RookieMaster <agrookie@gmail.com>
2018-01-28 12:48:22 +0200

    textInventory

--- gamejamproject/Assets/InventoryUI.cs
+++ gamejamproject/Assets/InventoryUI.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class InventoryUI : MonoBehaviour {
 
     public Text text;
-    public GameObject Player;
+    public GameObject m_player;
 
 
 
@@ -18,19 +18,16 @@ public class InventoryUI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        string textToOutput = @"Key X%d
+Axe X%d
+Artefact X%d
+";
+        Player player = m_player.GetComponent<Player>();
+        string output = string.Format(textToOutput, player.getNumberObjectiveInstances(Player.Objectives.KeyItem), 
+            player.getNumberObjectiveInstances(Player.Objectives.AxeItem), 
+            player.getNumberObjectiveInstances(Player.Objectives.ArtefactItem));
+        text.text = output;
 
-    IEnumerator updateGUI()
-    {
-        while (true)
-        {
-            string textToOutput = "";
-            foreach(string input in )
-
-
-
-        }
     }
 
 
