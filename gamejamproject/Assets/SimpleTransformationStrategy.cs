d0b8e7779955eb5b40e8cc58a6968a691ed33ad9
Jonas Valiunas <Jonas.Valiunas@bentley.com>
2018-01-28 14:42:54 +0200

    fix portal logic

--- gamejamproject/Assets/SimpleTransformationStrategy.cs
+++ gamejamproject/Assets/SimpleTransformationStrategy.cs
@@ -8,7 +8,8 @@ public class SimpleTransformationStrategy : TransformationStrategy
     public override void DoTransform(Player player, StepThroughPortal targetPortal)
     {
         Vector3 newPosition = targetPortal.transform.position + player.transform.forward * 1;
-        targetPortal.OrientToDirection(targetPortal.transform.position + player.transform.forward * 2);
+        targetPortal.OrientToDirection(player.GetComponentInChildren<Camera>().transform.forward);
         player.transform.position = newPosition;
+        player.GetComponentInChildren<Rigidbody>().velocity = new Vector3();
     }
 }
